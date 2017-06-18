f1ce53c23a1487a5c7abea04f96caf7d90d800d4
foresthuo <foresthuo@126.com>
2017-06-19 00:53:01 +0800

    Initial commit

---
+++ app/src/main/java/com/italentek/airschool/ClassroomActivity.java
@@ -0,0 +1,13 @@
+package com.italentek.airschool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classroom);
+    }
+}
